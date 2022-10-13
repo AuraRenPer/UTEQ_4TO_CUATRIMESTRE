d57c9ed2faf620bd2495346fa664825f1d8dbff5
AuraRenPer <114194390+AuraRenPer@users.noreply.github.com>
2022-10-13 08:18:36 -0500

    App02

--- app1/lib/home_screen.dart
+++ app1/lib/home_screen.dart
@@ -3,32 +3,71 @@
 
 import 'package:flutter/material.dart';
 
-class HolaScreen extends StatelessWidget {
+class HolaScreen extends StatefulWidget {
   const HolaScreen({super.key});
 
   static const colorBgAppBar = Colors.blueGrey;
-  static const colorTxtAppBar = Color.fromARGB(0, 248, 247, 247);
-  static const colorShadAppBar = Colors.redAccent;
+  static const colorTxtAppBar = Color(0xFFFFFFFF);
+  static const colorShadAppBar = Colors.black87;
 
+  @override
+  State<HolaScreen> createState() => _HolaScreenState();
+}
+
+class _HolaScreenState extends State<HolaScreen> {
+  int contador = 0;
+//los dos tipos de widgets: statelesswidget y statefulwidget
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Mi App"),
-        backgroundColor: Colors.lightGreen,
-        shadowColor: colorBgAppBar,
-        centerTitle: true,
-        titleTextStyle: const TextStyle(color: colorTxtAppBar, fontSize: 30),
-        leading: Padding(
-          padding: const EdgeInsets.all(0.0),
-          child: Image.asset(
-            "images/305163953054211.png",
-          ),
-        ),
+          title: const Text("Mi App"),
+          backgroundColor: HolaScreen.colorBgAppBar,
+          shadowColor: HolaScreen.colorShadAppBar,
+          centerTitle: true,
+          titleTextStyle:
+              const TextStyle(color: HolaScreen.colorTxtAppBar, fontSize: 30),
+          leading: Padding(
+              padding: const EdgeInsets.all(0.0),
+              child: Image.asset(
+                "images/drewhouse.png",
+              ))),
+      backgroundColor: const Color.fromARGB(133, 0, 0, 0),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                "Veces que haz hecho click",
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+              Text(
+                contador.toString(),
+                style: const TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontFamily: "Comic Sans"),
+              ),
+            ]),
       ),
-      backgroundColor: Color.fromARGB(255, 255, 252, 54),
-      body: const Center(
-        child: Text("Saludos desde HomeScreen"),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+              child: const Icon(Icons.add),
+              onPressed: () {
+                contador++;
+                setState(() {});
+              }),
+          FloatingActionButton(
+              child: const Icon(Icons.delete),
+              onPressed: () {
+                contador--;
+                setState(() {});
+              }),
+        ],
       ),
     );
   }
